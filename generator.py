f846ded18f4fb41b84af78158dcc50061dedde2e
Carlossb02 <99017078+Carlossb02@users.noreply.github.com>
2023-10-19 20:06:50 +0200

    Add files via upload

--- generator.py
+++ generator.py
@@ -43,6 +43,28 @@ def generar_palabra(longitud, solo_mins=False, solo_mayus=False,solo_nums=False,
         caracteres = string.ascii_lowercase + string.digits + string.ascii_uppercase
     return ''.join(random.choice(caracteres) for _ in range(random.choice(longitud)))
 
+def random_capitalize(word):
+    resultado = ""
+    for letra in word:
+        try: #Es un numero
+            num=int(letra)
+            resultado+=letra
+        except: #Es una letra
+            if random.choice([True, False]):  # Decidir aleatoriamente si convertir a mayúsculas
+                resultado += letra.upper()
+            else:
+                resultado += letra.lower()
+    return resultado
+
+def tiene_numero(word):
+    for letra in word:
+        try:
+            num=int(letra)
+            return True
+        except:
+            pass
+    return False
+
 # a) sha512
 for i in range(5):
     palabras_generadas = [generar_palabra([3,4,5,6,7], solo_mins=True) for _ in range(100)]
@@ -84,7 +106,7 @@ for i in range(5):
         for palabra in palabras_generadas:
             hash_obj = hashlib.sha512()
             hash_obj.update(palabra.encode())
-            palabra = base64.b64encode(hash_obj.digest()).decode()
+            palabra=hash_obj.hexdigest()
             archivo.write(palabra + "\n")
 
 ## b) md5crypt
@@ -112,7 +134,7 @@ for i in range(5):
         for palabra in palabras_generadas:
             hash_obj = hashlib.sha512()
             hash_obj.update(palabra.encode())
-            palabra = base64.b64encode(hash_obj.digest()).decode()
+            palabra=hash_obj.hexdigest()
             archivo.write(palabra + "\n")
 
 ## c) md5crypt
@@ -141,7 +163,7 @@ for i in range(5):
         for palabra in palabras_generadas:
             hash_obj = hashlib.sha512()
             hash_obj.update(palabra.encode())
-            palabra = base64.b64encode(hash_obj.digest()).decode()
+            palabra=hash_obj.hexdigest()
             archivo.write(palabra + "\n")
 
 ## d) md5crypt
@@ -162,39 +184,65 @@ for i in range(5):
 
 
 diccionario=datos="diccionario.txt"
-
+diccionario_letras={'a': ["@", "4"], 'e':["3"], 's':["5", "$"], 'o':["0"], 'l':["1"]}
 
 with open(datos, "r") as f:
     data = f.readlines()
     lengths = [3,4,5,6,7]
 
-    #e) sha512
+    #e) sha512 diccionario tipo 1
     for x in range (5):
         palabras_generadas = []
         for i in range(0,100):
             len_idx = random.randint(0,10) % len(lengths)
             word_idx = random.randint(0,10000) % len(data)
-            pwd = data[word_idx][:len_idx+1].strip()+str(random.randint(0,100))
+            pwd_txt = data[word_idx][:len_idx + 2].strip()
+            for letra in pwd_txt:
+                if letra in diccionario_letras.keys():
+                    pwd_txt= pwd_txt.replace(letra, random.choice([letra, random.choice(diccionario_letras[letra])]))
+
+            pwd1 = pwd_txt + str(random.randint(0,100))
+            pwd2 = str(random.randint(0,100)) + pwd_txt
+
+
+            if not tiene_numero(pwd_txt):
+                pwd = pwd_txt
+            else:
+                pwd = random.choice([pwd1,pwd2])
+
             palabras_generadas.append(pwd)
         #print(palabras_generadas)
         with open("contraseñas/e/sha512/" + str(x + 1) + ".txt", "w") as archivo:
             for pwd in palabras_generadas:
                 archivo.write(pwd + "\n")
         with open("contraseñas/e/sha512/" + str(x + 1) + "_hash.txt", "w") as archivo:
-            for pwd in palabras_generadas:
+            for palabra in palabras_generadas:
                 hash_obj = hashlib.sha512()
-                hash_obj.update(pwd.encode())
-                pwd = base64.b64encode(hash_obj.digest()).decode()
-                archivo.write(pwd + "\n")
+                hash_obj.update(palabra.encode())
+                palabra = hash_obj.hexdigest()
+                archivo.write(palabra + "\n")
 
 
-        # e) md5crypt
+        # e) md5crypt tipo 1
         for x in range(5):
             palabras_generadas = []
             for i in range(0, 100):
                 len_idx = random.randint(0, 10) % len(lengths)
                 word_idx = random.randint(0, 10000) % len(data)
-                pwd = data[word_idx][:len_idx + 1].strip() + str(random.randint(0, 100))
+                pwd_txt = data[word_idx][:len_idx + 2].strip()
+                for letra in pwd_txt:
+                    if letra in diccionario_letras.keys():
+                        pwd_txt = pwd_txt.replace(letra,
+                                                  random.choice([letra, random.choice(diccionario_letras[letra])]))
+
+                pwd1 = pwd_txt + str(random.randint(0, 100))
+                pwd2 = str(random.randint(0, 100)) + pwd_txt
+
+                if not tiene_numero(pwd_txt):
+                    pwd = pwd_txt
+                else:
+                    pwd = random.choice([pwd1, pwd2])
+
                 palabras_generadas.append(pwd)
             # print(palabras_generadas)
             with open("contraseñas/e/md5crypt/" + str(x + 1) + ".txt", "w") as archivo:
